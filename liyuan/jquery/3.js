626704ab3e00386bee312e221d10b48e03320fce
ly <3089916597@qq.com>
2019-08-13 23:58:57 +0800

    更新了jq文件

---
+++ liyuan/jquery/3.js
@@ -0,0 +1,9 @@
+// // $('p').appendTo('div');
+// $ ("<p/>").appendTo("div").addClass("test2");
+// $('p').prepend("<b>hello</b>");
+// $('p').prepend($(".foo")[1]);
+// console.log($('b'));
+// $('p').prependTo('#foo');
+// $("p").after("<b>ssss</b>");
+// $('p').after($('b')[0]);
+$('p').insertAfter("div");
